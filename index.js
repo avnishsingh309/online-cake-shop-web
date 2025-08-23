9ffeff97f084c97e9d99c05398d84c1756d12e81
Avnish Singh <avnishsinghrajput309@gmail.com>
2025-08-24 00:37:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector("#menu-btn");
+const slides = document.querySelectorAll(".home .slides-container .slide");
+const header = document.querySelector(".header");
+
+let index = 0;
+document.addEventListener("DOMContentLoaded",
+	function () {
+		const hamburger = document.getElementById("hamburger");
+		const navbar = document.querySelector(".navbar");
+
+		hamburger.addEventListener("click", function () {
+			navbar.classList.toggle("active");
+		});
+	});
+
+window.addEventListener("scroll", () => {
+	navbar.classList.remove("active");
+});
+
+function showSlide(nextIndex) {
+	slides[index].classList.remove("active");
+	index = (nextIndex + slides.length) % slides.length;
+	slides[index].classList.add("active");
+}
+
+function next() {
+	showSlide(index + 1);
+}
+
+function prev() {
+	showSlide(index - 1);
+}
